24b2a6d4413f02f3d6834065eb280969828f13b2
root <root@hing.me>
2017-12-01 07:32:53 +0000

    test flask

---
+++ run.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+print(Flask)
+
+@app.route('/')
+def index():
+    return '<h1>hello world</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
